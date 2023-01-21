6352c7df851380257c18c07fb040698a907e0165
Freddie <frederickfreund@gmail.com>
2023-01-21 20:44:50 +0100

    Add Sounds to cards

--- Assets/Scripts/UI/CardPack.cs
+++ Assets/Scripts/UI/CardPack.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
 public class CardPack : MonoBehaviour, IPointerClickHandler
 {
@@ -12,7 +14,16 @@ public class CardPack : MonoBehaviour, IPointerClickHandler
     [SerializeField] private Card cardPrefab;
     [SerializeField] private int cardAmount;
     [SerializeField] private float spawnPause;
-    
+    [SerializeField] private AudioClip cardSound1;
+    [SerializeField] private AudioClip cardSound2;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         StartCoroutine(OpenPack());
@@ -25,6 +36,8 @@ public class CardPack : MonoBehaviour, IPointerClickHandler
             var card = Instantiate(cardPrefab, bottomCardArea);
             var randomCardIndex = Random.Range(0, possibleCards.Count);
             card.SetValues(possibleCards.ElementAt(randomCardIndex));
+            _audioSource.clip = Random.Range(0, 2) > 0 ? cardSound1 : cardSound2;
+            _audioSource.Play();
             yield return new WaitForSeconds(spawnPause);
         }
     }
